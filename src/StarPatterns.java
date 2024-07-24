fd0f838224dd2d9e078530f6a8a617afc1d364ee
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-23 23:39:22 -0600

    Refactor star pattern program for improved performance

--- src/StarPatterns.java
+++ src/StarPatterns.java
@@ -8,6 +8,9 @@ public class StarPatterns {
 
         System.out.println(starPattern(num));
         System.out.println(starPattern2(num));
+        System.out.println(starPattern3(num));
+        System.out.println(numberPattern(num));
+        System.out.println(starPattern4(num));
 
     }
 
@@ -33,5 +36,46 @@ public class StarPatterns {
         return str.toString();
     }
 
-    
+    static String starPattern3(int num){
+        StringBuilder str = new StringBuilder();
+        for(int i = num; i>0; i--){
+            for (int j = 1; j <= i ; j++) {
+                str.append("*");
+            }
+            str.append("\n");
+        }
+        return str.toString();
+    }
+
+    static String numberPattern(int num){
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= num ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                str.append(j + " ");
+            }
+            str.append("\n");
+        }
+        return str.toString();
+    }
+
+    static String starPattern4(int num) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                str.append("*");
+            }
+            str.append("\n");
+        }
+        for(int i = num-1; i>0; i--){
+            for (int j = 1; j <= i; j++) {
+                str.append("*");
+            }
+            str.append("\n");
+        }
+
+        return str.toString();
+    }
+
+    //Let's do the same using one outer and one inner loop
+
 }
