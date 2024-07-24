d7363041f449281c30d23aef88c6da3347b81628
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-23 22:16:26 -0600

    chore: Refactor star pattern program to use StringBuilder for improved performance

--- src/StarPatterns.java
+++ src/StarPatterns.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class StarPatterns {
+import java.util.Scanner;
+
+public class StarPatterns {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        System.out.println(starPattern(num));
+        System.out.println(starPattern2(num));
+
+    }
+
+    static String starPattern(int num){
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i<num; i++){
+            for (int j = 0; j < num; j++) {
+                str.append("*");
+            }
+            str.append("\n");
+        }
+        return str.toString();
+    }
+
+    static String starPattern2(int num){
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i ; j++) {
+                 str.append("*");
+            }
+            str.append("\n");
+        }
+        return str.toString();
+    }
+
+    
 }
